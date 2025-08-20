55de4132af3f1c938780a80536d45b3b98dafcc5
Pawel Mizwa <pawel.mizwa@pragmaticcoders.com>
2025-08-20 15:39:02 +0200

    fix

--- src/views/About/PrivacyView.vue
+++ src/views/About/PrivacyView.vue
@@ -1,46 +1,283 @@
 <template>
-  <div class="max-w-screen-xl mx-auto px-4 md:px-16">
-    <h1>{{ t("Privacy Policy") }}</h1>
-    <div
-      class="prose dark:prose-invert"
-      v-if="config?.privacy"
-      v-html="config.privacy.bodyHtml"
-    />
+  <div
+    class="bg-white flex flex-col gap-8 items-start justify-start p-0 relative w-full max-w-[592px] mx-auto px-4 py-8"
+  >
+    <h1 class="font-bold leading-[40px] text-[#1c1b1f] text-[30px] w-full">
+      Polityka prywatności
+    </h1>
+
+    <div class="flex flex-col gap-4 w-full">
+      <div class="flex flex-col gap-2 w-full">
+        <p class="text-[17px] leading-[26px] text-[#1c1b1f]">
+          Niektóre terminy, techniczne lub inne, użyte w poniższym tekście mogą
+          obejmować pojęcia, które są trudne do zrozumienia. Udostępniliśmy
+          <a
+            href="https://meetup.pragmaticcoders.com/glossary"
+            class="font-bold underline"
+            >słowniczek</a
+          >, aby pomóc w ich lepszym zrozumieniu.
+        </p>
+      </div>
+    </div>
+
+    <div class="flex flex-col gap-4 w-full">
+      <div class="flex flex-col gap-2 w-full">
+        <h2 class="font-bold text-[20px] leading-[30px] text-[#1c1b1f]">
+          Jakie informacje zbieramy?
+        </h2>
+        <ul
+          class="list-disc ml-6 text-[17px] leading-[26px] text-[#1c1b1f] space-y-4"
+        >
+          <li>
+            <span class="font-bold">Podstawowe informacje o koncie</span><br />
+            Zbieramy informacje od Ciebie, gdy rejestrujesz się w tej instancji
+            i zbieramy dane, gdy uczestniczysz w platformie poprzez czytanie,
+            pisanie i interakcję z udostępnianymi tutaj treściami. Jeśli
+            zarejestrujesz się w tej instancji, zostaniesz poproszony o podanie
+            adresu e-mail, hasła (zaszyfrowanego) i co najmniej nazwy
+            użytkownika. Twój adres e-mail zostanie zweryfikowany za pomocą
+            wiadomości e-mail zawierającej unikalny link. Gdy link zostanie
+            aktywowany, wiemy, że kontrolujesz ten adres e-mail. Możesz także
+            wprowadzić dodatkowe informacje profilowe, takie jak wyświetlana
+            nazwa i biografia, a także przesłać zdjęcie profilowe i obraz w
+            nagłówku. Nazwa użytkownika(-czki), wyświetlana nazwa, biografia,
+            zdjęcie profilowe i obraz w nagłówku są zawsze widoczne publicznie.
+            Możesz jednak odwiedzić tę instancję bez rejestracji.
+          </li>
+          <li>
+            <span class="font-bold">Publikowane wydarzenia i komentarze</span
+            ><br />
+            Twoje zdarzenia i komentarze są dostarczane do innych instancji,
+            które obserwują Twoją, co oznacza, że są one dostarczane do różnych
+            instancji i tam są przechowywane ich kopie. Gdy usuwasz wydarzenia
+            lub komentarze, są one również dostarczane do tych innych instancji.
+            Wszystkie interakcje związane z funkcjami wydarzenia — takimi jak
+            dołączanie do wydarzenia — lub funkcjami grupowymi — takimi jak
+            zarządzanie zasobami — są również federacyjne. Należy pamiętać, że
+            operatorzy instancji i wszelkie instancje otrzymujące mogą
+            przeglądać takie wiadomości i informacje, a odbiorcy mogą tworzyć z
+            nich zrzuty ekranu, kopiować lub w inny sposób ponownie udostępniać.
+            <span class="font-bold"
+              >Nie udostępniaj żadnych niebezpiecznych informacji przez
+              Mobilizon.</span
+            >
+          </li>
+          <li>
+            <span class="font-bold">Adresy IP i inne metadane</span><br />
+            Możemy też przechowywać dzienniki serwera zawierające adresy IP
+            każdego żądania skierowanego na nasz serwer.
+          </li>
+        </ul>
+      </div>
+    </div>
+
+    <div class="flex flex-col gap-4 w-full">
+      <div class="flex flex-col gap-2 w-full">
+        <h2 class="font-bold text-[20px] leading-[30px] text-[#1c1b1f]">
+          Do czego używamy Twoich informacji?
+        </h2>
+        <p class="text-[17px] leading-[26px] text-[#1c1b1f] mb-2">
+          Dowolne informacje które zbieramy mogą zostać wykorzystane w
+          następujące sposoby:
+        </p>
+        <ul
+          class="list-disc ml-6 text-[17px] leading-[26px] text-[#1c1b1f] space-y-2"
+        >
+          <li>
+            Aby zapewnić podstawową funkcjonalność Mobilizon. Zgodnie z polityką
+            tej instancji, możliwe będzie wchodzenie w interakcje z treściami
+            innych osób i publikowanie własnych treści wyłącznie po zalogowaniu.
+          </li>
+          <li>
+            Aby pomóc w moderowaniu społeczności, na przykład porównując adres
+            IP użytkownika z innymi znanymi adresami w celu wykrywania unikania
+            bana lub innych naruszeń.
+          </li>
+          <li>
+            Podany przez Ciebie adres e-mail może być wykorzystywany do
+            wysyłania informacji, aktualizacji i powiadomień o innych osobach
+            interakcji z treściami użytkownika lub wysyłania wiadomości, a także
+            do odpowiadania na zapytania i/lub inne prośby albo pytania.
+          </li>
+        </ul>
+      </div>
+    </div>
+
+    <div class="flex flex-col gap-4 w-full">
+      <div class="flex flex-col gap-2 w-full">
+        <h2 class="font-bold text-[20px] leading-[30px] text-[#1c1b1f]">
+          Czy zabezpieczamy Twoje informacje?
+        </h2>
+        <p class="text-[17px] leading-[26px] text-[#1c1b1f]">
+          Wdrażamy różne środki bezpieczeństwa w celu zapewnienia bezpieczeństwa
+          danych osobowych użytkownika podczas ich wprowadzania, przesyłania lub
+          uzyskiwania do nich dostępu. Między innymi sesja przeglądarki, a także
+          ruch między aplikacjami użytkownika a interfejsem API są zabezpieczone
+          protokołem SSL/TLS, a hasło użytkownika jest szyfrowane silnym
+          algorytmem jednokierunkowym.
+        </p>
+      </div>
+    </div>
+
+    <div class="flex flex-col gap-4 w-full">
+      <div class="flex flex-col gap-2 w-full">
+        <h2 class="font-bold text-[20px] leading-[30px] text-[#1c1b1f]">
+          Jaka jest nasza polityka przechowywania danych?
+        </h2>
+        <p class="text-[17px] leading-[26px] text-[#1c1b1f] mb-2">
+          Będziemy dokonywać wszelkich starań, aby:
+        </p>
+        <ul
+          class="list-disc ml-6 text-[17px] leading-[26px] text-[#1c1b1f] space-y-2"
+        >
+          <li>
+            Przechowywać dzienniki serwera zawierające adresy IP wszystkich
+            żądań kierowanych do tego serwera, o ile takie dzienniki są
+            przechowywane, nie dłużej niż 90 dni.
+          </li>
+          <li>
+            Nie gromadzić adresów IP przypisanych do zarejestrowanych
+            użytkowników dłużej niż przez 12 miesięcy.
+          </li>
+        </ul>
+        <p class="text-[17px] leading-[26px] text-[#1c1b1f] mt-4">
+          Możesz bezpowrotnie usunąć swoje konto w dowolnym momencie.
+        </p>
+      </div>
+    </div>
+
+    <div class="flex flex-col gap-4 w-full">
+      <div class="flex flex-col gap-2 w-full">
+        <h2 class="font-bold text-[20px] leading-[30px] text-[#1c1b1f]">
+          Czy korzystamy z plików cookies?
+        </h2>
+        <p class="text-[17px] leading-[26px] text-[#1c1b1f] mb-2">
+          Przechowujemy następujące informacje o urządzeniu przez które łączysz
+          się:
+        </p>
+        <ul
+          class="list-disc ml-6 text-[17px] leading-[26px] text-[#1c1b1f] space-y-1"
+        >
+          <li>Wewnętrzne ID użytkownika</li>
+          <li>Wewnętrzne ID dla obecnie wybranej tożsamości</li>
+          <li>Tokeny do uwierzytelniania Ciebie</li>
+        </ul>
+        <p class="text-[17px] leading-[26px] text-[#1c1b1f] mt-4">
+          Jeżeli usuniesz te informacje, musisz zalogować się ponownie.
+        </p>
+        <p class="text-[17px] leading-[26px] text-[#1c1b1f] mt-4">
+          Jeśli nie jesteś połączony, nie przechowujemy żadnych informacji na
+          Twoim urządzeniu, chyba że uczestniczysz w wydarzeniu anonimowo. W tym
+          konkretnym przypadku przechowujemy skrót unikalnego identyfikatora
+          wydarzenia i statusu uczestnictwa w przeglądarce, abyśmy mogli
+          wyświetlić status uczestnictwa. Usunięcie tych informacji spowoduje
+          jedynie zaprzestanie wyświetlania statusu uczestnictwa w przeglądarce.
+        </p>
+        <p class="text-[17px] leading-[26px] text-[#1c1b1f] mt-4">
+          Uwaga: Informacje te są przechowywane w pamięci lokalnej, a nie w
+          plikach cookies
+        </p>
+      </div>
+    </div>
+
+    <div class="flex flex-col gap-4 w-full">
+      <div class="flex flex-col gap-2 w-full">
+        <h2 class="font-bold text-[20px] leading-[30px] text-[#1c1b1f]">
+          Czy przekazujemy jakieś informacje podmiotom trzecim?
+        </h2>
+        <p class="text-[17px] leading-[26px] text-[#1c1b1f]">
+          Nie, nie sprzedajemy, nie wymieniamy ani w żaden inny sposób nie
+          przekazujemy podmiotom zewnętrznym danych osobowych użytkowników. Nie
+          obejmuje to zaufanych stron trzecich, które pomagają nam w obsłudze
+          naszej witryny, prowadzeniu naszej działalności lub obsłudze
+          użytkownika, o ile strony te zgadzają się zachować poufność tych
+          informacji. Możemy również ujawnić dane użytkownika, gdy uznamy, że
+          jest to właściwe w celu przestrzegania prawa, egzekwowania zasad
+          naszej witryny lub ochrony praw, własności albo bezpieczeństwa naszego
+          lub innych osób.
+        </p>
+        <p class="text-[17px] leading-[26px] text-[#1c1b1f] mt-4">
+          Twoje treści mogą być pobierane przez inne instancje w sieci. Twoje
+          publiczne wydarzenia i komentarze są dostarczane do instancji
+          obserwujących Twoją instancję. Treść utworzona za pośrednictwem grupy
+          jest przekazywana do wszystkich instancji wszystkich członków(-iń)
+          grupy, o ile ci członkowie(-inie) znajdują się w innej instancji niż
+          ta.
+        </p>
+      </div>
+    </div>
+
+    <div class="flex flex-col gap-4 w-full">
+      <div class="flex flex-col gap-2 w-full">
+        <h2 class="font-bold text-[20px] leading-[30px] text-[#1c1b1f]">
+          Użycie strony przez dzieci
+        </h2>
+        <p class="text-[17px] leading-[26px] text-[#1c1b1f]">
+          Jeżeli serwer jest umiejscowiony w UE lub EOG – nasza strona, produkty
+          i usługi są przeznaczone dla osób mających przynajmniej 16 lat. Jeżeli
+          masz mniej niż 16 lat, zgodnie z wymogami RODO (<a
+            href="https://pl.wikipedia.org/wiki/Og%C3%B3lne_rozporz%C4%85dzenie_o_ochronie_danych"
+            class="font-bold underline"
+            >Ogólnego rozporządzenia o ochronie danych</a
+          >), nie używaj tej strony.
+        </p>
+        <p class="text-[17px] leading-[26px] text-[#1c1b1f] mt-4">
+          Jeżeli serwer jest umiejscowiony w USA – nasza strona, produkty i
+          usługi są przeznaczone dla osób mających przynajmniej 13 lat. Jeżeli
+          masz mniej niż 13 lat, zgodnie z wymogami COPPA (<a
+            href="https://pl.wikipedia.org/wiki/Children%27s_Online_Privacy_Protection_Act"
+            class="font-bold underline"
+            >Children's Online Privacy Protection Act</a
+          >), nie używaj tej strony.
+        </p>
+        <p class="text-[17px] leading-[26px] text-[#1c1b1f] mt-4">
+          Wymagania prawne mogą się różnić, jeżeli serwer jest pod inną
+          jurysdykcją.
+        </p>
+      </div>
+    </div>
+
+    <div class="flex flex-col gap-4 w-full">
+      <div class="flex flex-col gap-2 w-full">
+        <h2 class="font-bold text-[20px] leading-[30px] text-[#1c1b1f]">
+          Zmiany w naszej Polityce prywatności
+        </h2>
+        <p class="text-[17px] leading-[26px] text-[#1c1b1f]">
+          Jeżeli zdecydujemy się zmienić naszą politykę prywatności,
+          opublikujemy zmiany na tej stronie.
+        </p>
+        <p class="text-[17px] leading-[26px] text-[#1c1b1f] mt-4">
+          Ten dokument został opublikowany na licencji
+          <a
+            href="https://creativecommons.org/licenses/by-sa/4.0/"
+            class="font-bold underline"
+            >CC-BY-SA</a
+          >. Ostatnio był aktualizowany 18 czerwca 2020.
+        </p>
+        <p class="text-[17px] leading-[26px] text-[#1c1b1f] mt-4">
+          Oryginalnie oparto o teksty polityki prywatności
+          <a href="https://mastodon.social/terms" class="font-bold underline"
+            >Mastodona</a
+          >
+          i
+          <a href="https://joindiaspora.com/terms" class="font-bold underline"
+            >Diaspory*</a
+          >, również opublikowane na licencji
+          <a
+            href="https://creativecommons.org/licenses/by-sa/4.0/"
+            class="font-bold underline"
+            >CC BY-SA</a
+          >.
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { PRIVACY } from "@/graphql/config";
-import { IConfig } from "@/types/config.model";
-import { InstancePrivacyType } from "@/types/enums";
-import { useQuery } from "@vue/apollo-composable";
 import { useHead } from "@/utils/head";
-import { computed, watch } from "vue";
-import { useI18n } from "vue-i18n";
-
-const { locale } = useI18n({ useScope: "global" });
-
-const { result: configResult } = useQuery<{ config: IConfig }>(
-  PRIVACY,
-  () => ({
-    locale: locale,
-  }),
-  () => ({
-    enabled: locale !== undefined,
-  })
-);
-
-const config = computed(() => configResult.value?.config);
-
-const { t } = useI18n({ useScope: "global" });
 
 useHead({
-  title: t("Privacy Policy"),
-});
-
-watch(config, () => {
-  if (config.value?.privacy?.type === InstancePrivacyType.URL) {
-    window.location.replace(config.value?.privacy?.url);
-  }
+  title: "Polityka prywatności",
 });
 </script>
