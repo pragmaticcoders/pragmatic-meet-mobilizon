253adbebfa5c415d712e45efa84de9ccfa3401e2
Pawel Mizwa <pawel.mizwa@pragmaticcoders.com>
2025-08-25 18:41:45 +0200

    small style fix

--- src/views/Settings/PreferencesView.vue
+++ src/views/Settings/PreferencesView.vue
@@ -80,7 +80,7 @@
       <div class="space-y-4">
         <div class="flex gap-4">
           <div class="flex-1 max-w-md">
-            <label class="block font-medium text-[15px] text-[#1c1b1f] pb-4">
+            <label class="block font-medium text-[15px] text-[#1c1b1f] pb-2">
               {{ t("City") }}
             </label>
             <full-address-auto-complete
