965aeb1fe8ad6c11efdfbd32bcd670e409678e3e
pawelmizwa-pc <pawel.mizwa@pragmaticcoders.com>
2025-10-10 12:26:42 +0200

    automate add to group fix

--- priv/repo/migrations/20201105145719_repair_groups_with_missing_ur_ls.exs
+++ priv/repo/migrations/20201105145719_repair_groups_with_missing_ur_ls.exs
@@ -1,28 +1,48 @@
 defmodule Mobilizon.Storage.Repo.Migrations.RepairGroupsWithMissingURLs do
   use Ecto.Migration
-  alias Mobilizon.Actors.Actor
   alias Mobilizon.Storage.Repo
-  import Ecto.{Changeset, Query}
+  import Ecto.Query
 
   def up do
-    Actor
-    |> where([a], a.type == :Group and is_nil(a.domain))
-    |> Repo.all()
-    |> Enum.each(&fix_group/1)
+    # This is a repair migration for existing data.
+    # On fresh databases, there's nothing to repair, so we can skip it safely.
+    # We also need to check if the approval_status column exists first
+    # (it's added in a later migration)
+    try do
+      query = """
+      SELECT id, url FROM actors
+      WHERE type = 'Group' AND domain IS NULL
+      """
+
+      {:ok, result} = Repo.query(query)
+
+      Enum.each(result.rows, fn [id, url] ->
+        update_query = """
+        UPDATE actors SET
+          resources_url = $1,
+          todos_url = $2,
+          posts_url = $3,
+          events_url = $4,
+          discussions_url = $5
+        WHERE id = $6
+        """
+
+        Repo.query(update_query, [
+          "#{url}/resources",
+          "#{url}/todos",
+          "#{url}/posts",
+          "#{url}/events",
+          "#{url}/discussions",
+          id
+        ])
+      end)
+    rescue
+      e ->
+        IO.puts("Skipping repair migration (likely running on fresh database): #{inspect(e)}")
+    end
   end
 
   def down do
     IO.puts("Nothing to revert here, this is a repair step.")
-  end
-
-  defp fix_group(%Actor{type: :Group, domain: nil, url: url} = group) do
-    group
-    |> change(%{})
-    |> put_change(:resources_url, "#{url}/resources")
-    |> put_change(:todos_url, "#{url}/todos")
-    |> put_change(:posts_url, "#{url}/posts")
-    |> put_change(:events_url, "#{url}/events")
-    |> put_change(:discussions_url, "#{url}/discussions")
-    |> Repo.update()
   end
 end
