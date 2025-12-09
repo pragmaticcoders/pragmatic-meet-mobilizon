4a58f6c22dbd2c82931ef2c36b69767282f0ede8
Patryk Odziomek <odziomek.patryk@gmail.com>
2025-12-09 12:43:24 +0100

    fix: csp

--- config/docker.exs
+++ config/docker.exs
@@ -375,7 +375,7 @@ config :mobilizon, :http_security,
   sts_max_age: 31_536_000,
   csp_policy: [
     script_src: ["https://consent.cookiebot.com"],
-    style_src: ["'unsafe-inline'"],
+    style_src: ["'unsafe-inline' "],
     connect_src: [
       "https://consentcdn.cookiebot.com",
       "https://consent.cookiebot.com"
